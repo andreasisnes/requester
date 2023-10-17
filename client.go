8ae8aa85d5ea852a834c44ce265e48f1ace911c0
Andreas Isnes <andreas.isnes@gmail.com>
2023-10-17 16:38:39 +0200

    add comments

--- client.go
+++ client.go
@@ -15,6 +15,8 @@ type Client struct {
 
 type ClientOptions func(client *Client)
 
+// New initialize a default client.
+// Provide ClientOptions to modify default behavior
 func New(opts ...ClientOptions) *Client {
 	c := &Client{
 		Client: http.DefaultClient,
@@ -27,38 +29,59 @@ func New(opts ...ClientOptions) *Client {
 	return c
 }
 
+// WithClient sets client to given HTTP client instance
 func WithClient(httpClient *http.Client) ClientOptions {
 	return func(client *Client) {
 		client.Client = httpClient
 	}
 }
 
+// WithBaseURL sets a base URL which will be the prefix for
+// all outbound HTTP requests
 func WithBaseURL(url string) ClientOptions {
 	return func(client *Client) {
 		client.url = url
 	}
 }
 
+// DELETE creates a HTTP DELETE request with the given route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) DELETE(ctx context.Context, route ...string) *Request {
 	return c.Request(ctx, http.MethodDelete, route...)
 }
 
+// PUT creates a HTTP PUT request with the given route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) PUT(ctx context.Context, route ...string) *Request {
 	return c.Request(ctx, http.MethodPut, route...)
 }
 
+// GET creates a HTTP GET request with the given route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) GET(ctx context.Context, route ...string) *Request {
 	return c.Request(ctx, http.MethodGet, route...)
 }
 
+// POST creates a HTTP POST request with the given route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) POST(ctx context.Context, route ...string) *Request {
 	return c.Request(ctx, http.MethodPost, route...)
 }
 
+// PATCH creates a HTTP PATCH request with the given route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) PATCH(ctx context.Context, route ...string) *Request {
 	return c.Request(ctx, http.MethodPatch, route...)
 }
 
+// Request creates a HTTP request with the given HTTP method and route. If a base URL is specified
+// in client the given route should just contain the path otherwise the whole URL. The route
+// segments will be joined with / as seperator.
 func (c *Client) Request(ctx context.Context, method string, routes ...string) *Request {
 	uri, err := func() (string, error) {
 		if c.url == "" {
